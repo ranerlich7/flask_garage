e7ad86f2df97569fb74cbf2fce9a9aac02d48a0b
ranerlich7 <ranerlich@gmail.com>
2024-07-02 20:12:46 +0300

    added single car page

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-car1 = {"number": "123-456", "problems": []}
-car2 = {"number": "456-789", "problems": []}
+car1 = {
+    "id": "1",
+    "number": "123-456",
+    "problems": [],
+    "image": "https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&w=600",
+}
+car2 = {
+    "id": "2",
+    "number": "456-789",
+    "problems": [],
+    "image": "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=600",
+}
 cars = [car1, car2]
 
 
 @app.route("/")
 def cars_list():
-   return render_template('car_list.html', car_list=cars)
+    return render_template("car_list.html", car_list=cars)
 
     # final_str = ""
     # for car in cars:
@@ -18,11 +28,12 @@ def cars_list():
     # return final_str
 
 
-@app.route("/single_car/<int:index>")
-def single_car(index):
-    return (
-        f"<p>Number:{cars[index]['number']} <br> Problems:{cars[index]['problems']}</p>"
-    )
+@app.route("/single_car/<id>")
+def single_car(id):
+    for car in cars:
+        if car["id"] == id:
+            return render_template("single_car.html", car=car)
+    return render_template("single_car.html", car=None)
 
 
 @app.route("/add_car/")
